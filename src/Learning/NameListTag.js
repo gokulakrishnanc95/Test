bd631ada94c5e3e4616157044ddc890a3139d1f8
gokulakrishnanc95 <65064742+gokulakrishnanc95@users.noreply.github.com>
2020-07-21 19:56:47 +0530

    React App installed

--- src/Learning/NameListTag.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NameListTag({Names}){
-    return(
-        <h1>My Name is :{Names.name}. Age : {Names.age}. BloodGroup : {Names.bloodGroup}</h1>
-    )
-    
-}
-export default NameListTag
--- src/Learning/RenderRow.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RenderRow = (props) =>{
-    return props.keys.map((key, index)=>{
-        return <td key={props.data[key]}>{props.data[key]}</td>
-    })
-}
-
-export default RenderRow
